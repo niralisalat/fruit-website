b461fd448d470d7d76e6676d4ed6e91da0fe20c8
Nirali Salat <your_email@example.com>
2025-12-05 22:25:45 +0530

    updated cart system and UI

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
+// Open & close fruit panel
+const openCartBtn = document.getElementById("openCart");
+const panel = document.getElementById("fruitPanel");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("closePanel");
+
+openCartBtn.addEventListener("click", () => {
+    panel.style.display = "block";
+    overlay.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    panel.style.display = "none";
+    overlay.style.display = "none";
+});
+
+// Close when clicking outside
+overlay.addEventListener("click", () => {
+    panel.style.display = "none";
+    overlay.style.display = "none";
+});
+
+// Add-to-Cart notification
+let cartCount = 0;
+const cartMsg = document.getElementById("cartMsg");
+
+document.querySelectorAll(".addCart").forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        cartCount++;
+
+        cartMsg.style.display = "block";
+        cartMsg.innerText = "Added! Cart Items: " + cartCount;
+
+        setTimeout(() => cartMsg.style.display = "none", 1500);
+    });
+});
+
+// Swiper slider settings
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 6000,     // slow auto slide change
+        delay: 6000,
         disableOnInteraction: false,
     },
-    speed: 1200,         // slow smooth slide animation
+    speed: 1200,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
